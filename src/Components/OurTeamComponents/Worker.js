886231305c50b60ccf06894eb362f5d29ec02597
Jeremiah Faluyi <faluyijeremiah2018@gmail.com>
2021-12-30 17:34:06 +0100

    folder and components setup

---
+++ src/Components/OurTeamComponents/Worker.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Worker() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Worker
---
+++ src/Components/ProductsServices.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function ProductsServices() {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default ProductsServices
